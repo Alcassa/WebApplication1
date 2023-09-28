affa5fbce50ce27b13b26d2611218661564c2fca
Alcassa <galcassa49@gmail.com>
2023-09-28 16:10:32 -0300

    Criando nova view,controller,model

---
+++ WebApplication1/Controllers/ProfessorController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        // GET: ProfessorController
+        public ActionResult Index()
+        {
+            List<ProfessorModel> professores = new List<ProfessorModel>()
+            {
+                new ProfessorModel()
+                {
+                    Nome="Alex",
+                    Id=1,
+                    Aulas=50
+                },
+                new ProfessorModel()
+                {
+                    Nome="Anderson",
+                    Id=2,
+                    Aulas=100
+                }
+            };
+            return View(professores);
+        }
+
+        // GET: ProfessorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProfessorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProfessorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfessorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProfessorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfessorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProfessorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/ProfessorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class ProfessorModel
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public int Aulas { get; set; }
+        public string Materia { get; set; }
+
+    }
+}
